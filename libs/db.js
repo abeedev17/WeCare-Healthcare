8d2365e19e73dfcd2ca27981a73d75fb8d0fa5f5
abeedev17 <as.asuperstar05@gmail.com>
2022-10-05 01:09:02 -0400

    attempt to deploy to vercel

--- libs/db.js
+++ libs/db.js
@@ -1,5 +1,6 @@
 import {connect,connection} from 'mongoose'
 
+const MONG0_URI = process.env.MONG0_URI
 const conn = {
     isConnected : false
 }
@@ -8,5 +9,14 @@ export async function dbConnect(){
     if(conn.isConnected) 
     return; 
 
-    const db = await connect(process.env.MONG_URI)
+    const db = await connect(MONG0_URI)
+
+    conn.isConnected = db.connections[0].readyState
 }
+
+connection.on('connected', () => {
+    console.log('Connected to Mongo database')
+});
+connection.on('error', (err) => {
+    console.log('Mongo database error', err.message)
+});
